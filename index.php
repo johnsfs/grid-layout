7e4789a4283264a67f3814b34eaf2eb053c22694
johnsfs <john@sfscollect.com>
2018-11-08 16:06:23 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GRIDZ</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <ul class="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Jobs</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
